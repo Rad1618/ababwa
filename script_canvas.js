f8d6ea42ca16149dfcd0913b3ec3e0c31c932f6d
Rad1618 <81822443+Rad1618@users.noreply.github.com>
2021-04-09 16:38:04 +0200

    Update script_canvas.js

--- script_canvas.js
+++ script_canvas.js
@@ -1,11 +1,11 @@
 // JavaScript source code
 function Draw()
 {
-    Draw_board();   //rysowanie planszy
+    Draw_board(null, null);   //rysowanie planszy
     Draw_dice(6, 0);   //rysowanie kości z sześcioma oczkami
 }
 
-function Draw_board() 
+function Draw_board(pawns_mode, fraction) 
 {
     if (DOM.board.getContext) 
     {
@@ -28,91 +28,19 @@ function Draw_board()
             ctx.textBaseline = 'middle';
             ctx.fillText('START', 300, 300, 100);
         }
-        ctx.strokeRect(275, 75, 50, 200); //środek planszy
-        ctx.strokeRect(75, 275, 200, 50);
-        ctx.strokeRect(275, 325, 50, 200);
-        ctx.strokeRect(325, 275, 200, 50);
 
-        ctx.fillStyle = 'rgb(0, 180, 225)';
-        ctx.fillRect(25, 175, 50, 50);
-        ctx.strokeRect(25, 175, 50, 50);    //brzeg Ali
-        ctx.strokeRect(75, 175, 50, 50);
-        ctx.strokeRect(125, 175, 50, 50);
-        ctx.strokeRect(175, 175, 50, 50);
-        ctx.strokeRect(175, 125, 50, 50);
-        ctx.strokeRect(175, 75, 50, 50);
-        ctx.strokeRect(175, 25, 50, 50);
-        ctx.strokeRect(225, 25, 50, 50);
-        ctx.strokeRect(275, 25, 50, 50);
-        ctx.strokeRect(325, 25, 50, 50);
+        for (var i = 0; i < 4; i++) //pola bezpieczne (kolorowe wnętrze)
+        {
+            ctx.fillStyle = Fraction_color(i);
+            ctx.fillRect(tables.spaces[i * 10][0], tables.spaces[i * 10][1], 50, 50);
+            Arrow(ctx, tables.spaces[i * 10][0], tables.spaces[i * 10][1], (i + 1) % 4, 'rgb(255, 200, 0)');       //rysowanie strzałki początkowej
+            Arrow(ctx, tables.spaces[i * 10 + 8][0], tables.spaces[i * 10 + 8][1], (i + 2) % 4, Fraction_color((i + 1) % 4));    //rysowanie strzałki końcowej
+        }
 
-        ctx.fillStyle = 'rgb(0, 220, 0)';
-        ctx.fillRect(375, 25, 50, 50);
-        ctx.strokeRect(375, 25, 50, 50);    //brzeg Gildia
-        ctx.strokeRect(375, 75, 50, 50);
-        ctx.strokeRect(375, 125, 50, 50);
-        ctx.strokeRect(375, 175, 50, 50);
-        ctx.strokeRect(425, 175, 50, 50);
-        ctx.strokeRect(475, 175, 50, 50);
-        ctx.strokeRect(525, 175, 50, 50);
-        ctx.strokeRect(525, 225, 50, 50);
-        ctx.strokeRect(525, 275, 50, 50);
-        ctx.strokeRect(525, 325, 50, 50);
+        for (var i = 0; i < 72; i++)    //rysowanie obramówek pól
+            ctx.strokeRect(tables.spaces[i][0], tables.spaces[i][1], 50, 50);
 
-        ctx.fillStyle = 'rgb(220, 0, 0)';
-        ctx.fillRect(525, 375, 50, 50);
-        ctx.strokeRect(525, 375, 50, 50);    //brzeg Harb
-        ctx.strokeRect(475, 375, 50, 50);
-        ctx.strokeRect(425, 375, 50, 50);
-        ctx.strokeRect(375, 375, 50, 50);
-        ctx.strokeRect(375, 425, 50, 50);
-        ctx.strokeRect(375, 475, 50, 50);
-        ctx.strokeRect(375, 525, 50, 50);
-        ctx.strokeRect(325, 525, 50, 50);
-        ctx.strokeRect(275, 525, 50, 50);
-        ctx.strokeRect(225, 525, 50, 50);
-
-        ctx.fillStyle = 'rgb(128, 0, 127)';
-        ctx.fillRect(175, 525, 50, 50);
-        ctx.strokeRect(175, 525, 50, 50);    //brzeg Marva
-        ctx.strokeRect(175, 475, 50, 50);
-        ctx.strokeRect(175, 425, 50, 50);
-        ctx.strokeRect(175, 375, 50, 50);
-        ctx.strokeRect(125, 375, 50, 50);
-        ctx.strokeRect(75, 375, 50, 50);
-        ctx.strokeRect(25, 375, 50, 50);
-        ctx.strokeRect(25, 325, 50, 50);
-        ctx.strokeRect(25, 275, 50, 50);
-        ctx.strokeRect(25, 225, 50, 50);
-
-        ctx.strokeRect(25, 50, 50, 50);     //pola startowe
-        ctx.strokeRect(75, 50, 50, 50);
-        ctx.strokeRect(25, 100, 50, 50);
-        ctx.strokeRect(75, 100, 50, 50);
-
-        ctx.strokeRect(450, 25, 50, 50);
-        ctx.strokeRect(450, 75, 50, 50);
-        ctx.strokeRect(500, 25, 50, 50);
-        ctx.strokeRect(500, 75, 50, 50);
-
-        ctx.strokeRect(475, 450, 50, 50);
-        ctx.strokeRect(475, 500, 50, 50);
-        ctx.strokeRect(525, 450, 50, 50);
-        ctx.strokeRect(525, 500, 50, 50);
-
-        ctx.strokeRect(50, 475, 50, 50);
-        ctx.strokeRect(50, 525, 50, 50);
-        ctx.strokeRect(100, 475, 50, 50);
-        ctx.strokeRect(100, 525, 50, 50);
-
-        Arrow(ctx, 25, 175, 'E', 'rgb(255, 200, 0)');   //rysowanie strzałek
-        Arrow(ctx, 375, 25, 'S', 'rgb(255, 200, 0)');
-        Arrow(ctx, 525, 375, 'W', 'rgb(255, 200, 0)');
-        Arrow(ctx, 175, 525, 'N', 'rgb(255, 200, 0)');
-        Arrow(ctx, 275, 25, 'S', 'rgb(0, 220, 0)');
-        Arrow(ctx, 525, 275, 'W', 'rgb(220, 0, 0)');
-        Arrow(ctx, 275, 525, 'N', 'rgb(128, 0, 127)');
-        Arrow(ctx, 25, 275, 'E', 'rgb(0, 180, 225)');
+        Draw_pawns(ctx, pawns_mode, fraction);    //rysowanie pionków
     }
 }
 
@@ -171,6 +99,45 @@ function Draw_dice(number, rolls)
     }
 }
 
+function Draw_pawns(ctx, mode, fraction)
+{
+    for (var i = 0; i < data_public.pawns.length; i++)
+    {
+        if (i % data_public.ppp === 0)
+            ctx.fillStyle = Fraction_color(i / data_public.ppp);
+        Circle(ctx, data_public.pawns[i].position[0] + 25, data_public.pawns[i].position[1] + 25, 15);
+        if (i < data_public.ppp)
+            data_private.can_move_pawn[i] = false;  //blokowanie możliwości ruchu
+    }
+    ctx.fillStyle = 'rgb(0, 0, 0)';
+    if (mode === 'roll_six' || mode === 'normal_move')  //możliwość ruchu pionkami po rzucie
+    {
+        ctx.fillStyle = 'rgba(255, 242, 0, 0.75)';
+        for (var i = 0; i < data_public.ppp; i++)
+        {
+            if (data_public.pawns[fraction * data_public.ppp + i].state === 'play')
+            {
+                Dot(ctx, data_public.pawns[fraction * data_public.ppp + i].position[0] + 25, data_public.pawns[fraction * data_public.ppp + i].position[1] + 25, 18);
+                data_private.can_move_pawn[i] = true;   //może ruszyć się tym pionkiem
+            }
+        }
+        ctx.fillStyle = 'rgb(0, 0, 0)';
+    }
+    if (mode === 'roll_six' && fraction != -1)    //rysowanie po wyrzuceniu 6
+    {
+        ctx.fillStyle = 'rgba(255, 242, 0, 0.75)';
+        for (var i = 0; i < data_public.ppp; i++)
+        {
+            if (data_public.pawns[fraction * data_public.ppp + i].state === 'home')
+            {
+                Dot(ctx, data_public.pawns[fraction * data_public.ppp + i].position[0] + 25, data_public.pawns[fraction * data_public.ppp + i].position[1] + 25, 18);
+                data_private.can_move_pawn[i] = true;   //może ruszyć się tym pionkiem
+            }
+        }
+        ctx.fillStyle = 'rgb(0, 0, 0)';
+    }
+}
+
 function Arrow(ctx, start_x, start_y, obr, color)
 {
     var bufor_strokeStyle = ctx.strokeStyle;
@@ -180,22 +147,22 @@ function Arrow(ctx, start_x, start_y, obr, color)
     ctx.lineWidth = 5;
     switch (obr)        //ustawianie parametrów, dla różnych obrotów
     {
-        case 'E':
+        case 1: //E
             x = x1 = y1 = 12;
             start_x += 12;
             start_y += 25;
             break;
-        case 'W':
+        case 3: //W
             x = x1 = y1 = -12;
             start_x += 38;
             start_y += 25;
             break;
-        case 'S':
+        case 2: //S
             y = y1 = x1 = 12;
             start_x += 25;
             start_y += 12;
             break;
-        case 'N':
+        case 0: //N
             y = y1 = x1 = -12;
             start_x += 25;
             start_y += 38;
@@ -207,7 +174,7 @@ function Arrow(ctx, start_x, start_y, obr, color)
     //ctx.moveTo(start_x + 2 * x, start_y + 2 * y);   //potrzebne, aby grot był wygładzony
     ctx.lineTo(start_x + x1, start_y + y1);  //rysowanie grotu strzałki
     ctx.moveTo(start_x + 2 * x, start_y + 2 * y);
-    if (obr === 'W' || obr === 'E')
+    if (obr === 1 || obr === 3)
         ctx.lineTo(start_x + x1, start_y - y1);
     else
         ctx.lineTo(start_x - x1, start_y + y1);
@@ -221,4 +188,12 @@ function Dot(ctx, poz_x, poz_y, radius)
     ctx.beginPath();
     ctx.arc(poz_x, poz_y, radius, 0, 2 * Math.PI, true);
     ctx.fill();
+}
+
+function Circle(ctx, poz_x, poz_y, radius)
+{
+    ctx.beginPath();
+    ctx.arc(poz_x, poz_y, radius, 0, 2 * Math.PI, true);
+    ctx.fill();
+    ctx.stroke();
 }
