ae7ace1e310c6b495d6ac9168c88b2a936706021
Rad1618 <81822443+Rad1618@users.noreply.github.com>
2021-04-18 14:39:26 +0200

    Create text.js

---
+++ text.js
@@ -0,0 +1,41 @@
+// JavaScript source code
+
+function Make_endings()
+{
+    tables.endings[0] = 'Księciu Aliemu udało się obronić Ababwę! Powrócił do swego pałacu, jako triumfator. Następnie rozpoczął umacnianie potęgi ' +
+        'Ababwy poprzez szereg reform. Zakończył kryzys ekonomiczny i zmodernizował wojsko. Powiększył również wrota do swojego pałacu, aby ludzie ' +
+        'nie musieli przeciskać się przez nie pojedyńczo. Dzięki poczyniamiom Księcia Aliego Ababwa stała się najpotężniejszym księstwem w regionie, ' +
+        'a historia zapamieta go, jako \"Niezwyciężonego\".';
+    tables.endings[1] = 'Księciu Aliemu udało się obronić Ababwę! Przynajmniej tak się mu zdawało. Zwycięsko wszedł do swej sali tronowej i nagle się ' +
+        'zatrzymał. Wokół niego stały zakapturzone postaci. Jego własni adiutanci nagle zamknęli za nim drzwi, był w pułapce. Najbardziej zakapturzona ' +
+        'z postaci przemówiła: "Od teraz to my tu będziemy rządzić! Ty będziesz jedynie naszą małą marionetką." Książę Ali nie miał wyjścia i musiał ' +
+        'przystać na ten układ. Jego zwycięstwo okazało się jedynie chwilowe.';
+    tables.endings[2] = 'Kupcy dotarli do pałacu i przekupili służbę. Następnie dzięki pieniądzom ze skarbca przekabacili na swoją stronę dowódców ' +
+        'wojsk pozostałych frakcji. Gdy wrogie siły ostatecznie się poddały, Gildia wprowadziła w Ababwie oligarchię. Dzięki sprawnej polityce ' +
+        'pieniężnej miasto wkrótce stało się potężnym punktem handlowym i zaczęło opływać w bogactwa. Dzięki inwestycją w naukę, kupcy niedługo ' +
+        'później odkryli Bitcoiny i stali się panami rynku światowego.';
+    tables.endings[3] = 'Kupcy dotarli do pałacu i przekupili służbę. Następnie dzięki pieniądzom ze skarbca przekabacili na swoją stronę dowódców ' +
+        'wojsk pozostałych frakcji. Gdy wrogie siły ostatecznie się poddały, Gildia wprowadziła w Ababwie oligarchię. Na nieszczęście dla kupców ' +
+        'okazało się, że większość przedstawicieli nowej rady należało do miejscowej loży masońskiej. Masoni szybko przekształcili Ababwę na swoją modłę, ' +
+        'a jej mieszkońców zesłali na ciężką pracę do kopalni Bitcoinów.';
+    tables.endings[4] = 'Wojownicy Pańswta Al\'Harb bez trudu pokonali swoich wrogów. Po przejęciu władzy w Ababwie, utworzyli z niej swoje państwo ' +
+        'satelickie z marionetkowym władcą na czele. Bogate w zasoby naturalne ziemie Ababwy zaczęły być intensywnie eksploatowane, a pozyskiwane ' +
+        'surowce wysyłane do stolicy Al\'Harb. Potęga wojowników, którzy nie poprzestali swoich podbojów na Ababwie, rosła i wkrótce w całym znanym ' +
+        'świecie zaczęto mówić o nowym, rozrastającym się, Imperium';
+    tables.endings[5] = 'Wojownicy Państwa Al\'Harb bez trudu pokonali swoich wrogów. Jednak nie spostrzegli, że niektórzy dowódcy zostali perfekcyjnie ' +
+        'podmienieni. Nikt nie przypuszczał, że nagły wzrost mody na głębokie kaptury wśród wodzów może oznaczać coś groźnego. W ten sposób Masoni ' +
+        'przedostali się na dwór sułtana Al\'Harb, gdzie będąc jego doradcami, zyskali olbrzymią władzę. Już niedługo dzięki całej sile wojskowej ' +
+        'Al\'Harb będą mogli zyskać władzę nad światem.';
+    tables.endings[6] = 'Córka Dżafara osiągnęła swój cel: pokonała Księcia Aliego. Teraz mogła dokonać swojej zemsty za ojca. Spaliła miasto, a ruiny ' +
+        'zrównała z ziemię. Żyzne ziemie zamieniła w pustynie, a rzeki w suche koryta. Mieszkańcy Ababwy, musieli emigrować do innych krajów. ' +
+        'Natomiast Marva, usatysfakcjonowana swoją zemstą, powróciła ze swoimi zwolennikami tam, skąd przybyła i resztę swojego życia spędziła ' +
+        'na pisaniu powieści przygodowych.';
+    tables.endings[7] = 'Córka Dżafara osiągnęła swój cel: pokonała Księcia Aliego. Teraz mogła dokonać swojej zemsty za ojca. Nie wiedziała jednak, że ' +
+        'wśród jej najbardziej zaufanych doradców kryli się Masoni. Gdy Marva pławiła się w zniszczeniu, Wolnomularze wykradli magiczną lampę. Ich pierwszym ' +
+        'życzeniem była władza nad światem, drugim zniesienie wszystkich ograniczeń dotyczących możliwych życzeń, a trzecim 50 kolejnych życzeń, na dalsze ' +
+        'projekty. I w ten sposób władza Masonów utrzymuje się po dzień dzisiejszy.';
+    tables.endings[8] = 'Darujmy sobie kwestię podziału na frakcje. Co najmniej 90% ludzi w pałacu to Masoni! Ali, Gildio, Harbie, Marvo, muszę Wam z ' +
+        'całego serca podziękować. Teraz nie ma takiej siły we wszechświecie, która byłaby w stanie powstrzymać takiego stężenia masonerii w jednym miejscu. ' +
+        'Władza nad światem to pikuś. Od teraz wyruszamy w kosmos, ostatnią granicę! Nie ma takiej cywilizacji, która się nam opsze. 90% Masonów ma w skali ' +
+        'Kardaszewa typ X. Z wyrazami szacunku, Wielki Mason, twórca gry :D';
+}
